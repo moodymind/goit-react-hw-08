dab6c9f21165d3f3b85d7df4f401531c24d3b8ae
Austin Miller <vadim.sukhetskii@gmail.com>
2024-08-14 12:42:19 +0200

    fixed 404 error in operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL =
-  "https://66bc753424da2de7ff6a9bbc.mockapi.io/contactList/";
+axios.defaults.baseURL = "https://66bc753424da2de7ff6a9bbc.mockapi.io";
 
 export const fetchContacts = createAsyncThunk(
-  "contacts/fetchAll",
+  "contactList/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("/contacts");
+      const response = await axios.get("/contactList");
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -17,10 +16,10 @@ export const fetchContacts = createAsyncThunk(
 );
 
 export const addContact = createAsyncThunk(
-  "contacts/addContact",
+  "contactList/addContact",
   async (newContact, thunkAPI) => {
     try {
-      const response = await axios.get("/contacts", { newContact });
+      const response = await axios.get("/contactList", { newContact });
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -29,10 +28,10 @@ export const addContact = createAsyncThunk(
 );
 
 export const deleteContact = createAsyncThunk(
-  "contacts/deleteContact",
+  "contactList/deleteContact",
   async (contactId, thunkAPI) => {
     try {
-      const response = await axios.delete(`/contacts/${contactId}`);
+      const response = await axios.delete(`/contactList/${contactId}`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);

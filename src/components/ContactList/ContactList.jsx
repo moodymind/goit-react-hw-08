d7f18725954d186eb638617171eb28ea36339ee7
Austin Miller <vadim.sukhetskii@gmail.com>
2024-08-17 12:15:54 +0200

    Small update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Contact } from "../Contact/Contact";
+import { Contact } from "../Contact /Contact";
 import { useSelector } from "react-redux";
 import { selectFilteredContacts } from "../../redux/filters/selectors";
 import styles from "./ContactList.module.css";
